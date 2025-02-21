15ef0e40b8a49e0f37c0fdff148d05d8623d5147
leehyunwoo <archihw94@gmail.com>
2025-02-21 10:10:11 +0900

    feat: window size hook

---
+++ src/hooks/useWindowSize.tsx
@@ -0,0 +1,28 @@
+import { MAX_WIDTH } from "@/constants";
+import { useEffect, useState } from "react";
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: Math.min(window.innerWidth, MAX_WIDTH),
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+};
+
+export default useWindowSize;
