cd9136358c18c2b8059ea49045c7ee4aead52dde
leehyunwoo <archihw94@gmail.com>
2025-02-21 10:11:57 +0900

    etc: 더미 데이터 및 상수 설정

---
+++ src/dummy/tabData.ts
@@ -0,0 +1,31 @@
+export interface ITap {
+  id: number;
+  name: string;
+}
+
+export const tabData: Array<ITap> = [
+  {
+    id: 1,
+    name: "차트",
+  },
+  {
+    id: 2,
+    name: "Whook",
+  },
+  {
+    id: 3,
+    name: "이벤트",
+  },
+  {
+    id: 4,
+    name: "뉴스",
+  },
+  {
+    id: 5,
+    name: "스토어",
+  },
+  {
+    id: 6,
+    name: "충전소",
+  },
+];
