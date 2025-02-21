18447a9c881754586e9d9865d82d9a766840b278
leehyunwoo <archihw94@gmail.com>
2025-02-21 10:12:32 +0900

    etc: image config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,18 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    domains: ["resource.hanteochart.io", "s3.ap-northeast-2.amazonaws.com"],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "resource.hanteochart.io",
+    //     port: "",
+    //     pathname: "/hanteonews/article/thumbnail/**",
+    //     search: "",
+    //   },
+    // ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div>한터글로벌</div>;
-}
